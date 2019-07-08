3b07b7bd9f2f8fe059bfb6b4b7b0853ef9b85847
Tarje Lavik <tarje.lavik@gmail.com>
2019-07-08 15:01:23 +0200

    Initial commit

---
+++ studio/README.md
@@ -0,0 +1,1 @@
+# saelen-samlingen-studio
---
+++ studio/dashboardConfig.js
@@ -0,0 +1,53 @@
+export default {
+  widgets: [
+    {
+      name: 'sanity-tutorials',
+      options: {
+        templateRepoId: 'sanity-io/sanity-template-gatsby-blog'
+      }
+    },
+    {name: 'structure-menu'},
+    {
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              description:
+                'NOTE: Because these sites are static builds, they need to be re-deployed to see the changes when documents are published.',
+              sites: [
+                {
+                  buildHookId: '5d233ea0dd15905f59bc18a4',
+                  title: 'Sanity Studio',
+                  name: 'saelen-samlingen-studio',
+                  apiId: '8650882c-97db-4284-bf66-242850629add'
+                },
+                {
+                  buildHookId: '5d233ea0c09ad50a72aed231',
+                  title: 'Blog Website',
+                  name: 'saelen-samlingen',
+                  apiId: 'ffc4bb67-3ecc-4379-993e-25b7187a116b'
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/tarjelavik/saelen-samlingen',
+            category: 'Code'
+          },
+          {title: 'Frontend', value: 'https://saelen-samlingen.netlify.com', category: 'apps'}
+        ]
+      }
+    },
+    {name: 'project-users', layout: {height: 'auto'}},
+    {
+      name: 'document-list',
+      options: {title: 'Recent blog posts', order: '_createdAt desc', types: ['post']},
+      layout: {width: 'medium'}
+    }
+  ]
+}
