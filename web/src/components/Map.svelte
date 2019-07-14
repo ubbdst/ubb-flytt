c0dddda37313aa6d56debe2822eb21fc56b06ada
Tarje Lavik <tarje.lavik@gmail.com>
2019-07-14 22:44:10 +0200

    Add icons to map

--- web/src/components/Map.svelte
+++ web/src/components/Map.svelte
@@ -43,15 +43,46 @@
           data: geojson
         })
 
-        map.addLayer({
-          "id": "points",
-          "type": "circle",
-          "source": "features",
-          "paint": {
-            "circle-radius": 6,
-            "circle-color": "#B42222"
-          },
-          "filter": ["==", "$type", "Point"],
+        map.loadImage('/240px-Camera_Flat_Icon_Vector.svg.png', function(error, image) {
+          if (error) throw error;
+          map.addImage('camera', image);
+          map.addLayer({
+            "id": "camera",
+            "type": "symbol",
+            "source": "features",
+            "layout": {
+              "text-field": "Kamera",
+              "icon-image": "camera",
+              "icon-size": 0.2,
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 0.6],
+              "text-anchor": "top"
+            },
+            "filter": ["==", "type", "Point of camera"],
+          });
+        });
+
+        map.loadImage('/Picture_icon-72a7cf.svg.png', function(error, image) {
+          if (error) throw error;
+          map.addImage('motif', image);
+          map.addLayer({
+            "id": "motif",
+            "type": "symbol",
+            "source": "features",
+            "layout": {
+              "text-field": "Motiv",
+              "icon-image": "motif",
+              "icon-size": 0.1,
+              "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+              "text-offset": [0, 0.6],
+              "text-anchor": "top"
+            },
+            "point": {
+              "circle-color": "#888",
+              "circle-radius": 6
+            },
+            "filter": ["==", "type", "Center of motif"],
+          });
         });
       })
     };
