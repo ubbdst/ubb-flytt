57d4ef1ab9d5f58be4d6f6935b3a6bea263e9d73
Tarje Lavik <tarje.lavik@gmail.com>
2019-07-15 00:23:58 +0200

    Clean up

--- studio/schemas/texts/genericText.js
+++ studio/schemas/texts/genericText.js
@@ -1,71 +1,71 @@
 import React from 'react'
 
 const highlightIcon = () => (
-  <span style={{ fontWeight: 'bold' }}>H</span>
+  <span style={{fontWeight: 'bold'}}>H</span>
 )
 const highlightRender = props => (
-  <span style={{ backgroundColor: 'yellow' }}>{props.children}</span>
+  <span style={{backgroundColor: 'yellow'}}>{props.children}</span>
 )
 
 export default {
-    name: 'genericText',
-    type: 'array',
-    title: 'Excerpt',
-    of: [
-        {
-            title: 'Block',
-            type: 'block',
-            styles: [
-                {title: 'Normal', value: 'normal'},
-                {title: 'H1', value: 'h1'},
-                {title: 'H2', value: 'h2'},
-                {title: 'Quote', value: 'blockquote'}
-            ],
-            lists: [
-                {title: 'Numbered', value: 'number'},
-                {title: 'Bulleted', value: 'bullet'}
-            ],
-            marks: {
-                decorators: [
-                    { title: 'Strong', value: 'strong' },
-                    { title: 'Emphasis', value: 'em' },
-                    {
-                        title: 'Highlight',
-                        value: 'highlight',
-                        blockEditor: {
-                            icon: highlightIcon,
-                            render: highlightRender
-                        }
-                      },
-                    { title: 'Code', value: 'code' }
-                ],
-                annotations: [
-                    {
-                        name: 'link',
-                        type: 'object',
-                        title: 'URL',
-                        fields: [
-                            {
-                                title: 'URL',
-                                name: 'href',
-                                type: 'url'
-                            }
-                        ]
-                    },
-                ]
+  name: 'genericText',
+  type: 'array',
+  title: 'Excerpt',
+  of: [
+    {
+      title: 'Block',
+      type: 'block',
+      styles: [
+        {title: 'Normal', value: 'normal'},
+        {title: 'H1', value: 'h1'},
+        {title: 'H2', value: 'h2'},
+        {title: 'Quote', value: 'blockquote'}
+      ],
+      lists: [
+        {title: 'Numbered', value: 'number'},
+        {title: 'Bulleted', value: 'bullet'}
+      ],
+      marks: {
+        decorators: [
+          {title: 'Strong', value: 'strong'},
+          {title: 'Emphasis', value: 'em'},
+          {
+            title: 'Highlight',
+            value: 'highlight',
+            blockEditor: {
+              icon: highlightIcon,
+              render: highlightRender
             }
-        },
-        {
-            type: 'reference',
-            to: [
-                { type: 'actor' },
-                { type: 'madeObject' },
-                { type: 'collection' },
-                { type: 'group' },
-                { type: 'event' },
-                { type: 'material' },
-                { type: 'timeline' }
+          },
+          {title: 'Code', value: 'code'}
+        ],
+        annotations: [
+          {
+            name: 'link',
+            type: 'object',
+            title: 'URL',
+            fields: [
+              {
+                title: 'URL',
+                name: 'href',
+                type: 'url'
+              }
             ]
-        }
-    ]
+          }
+        ]
+      }
+    },
+    {
+      type: 'reference',
+      to: [
+        {type: 'actor'},
+        {type: 'madeObject'},
+        {type: 'collection'},
+        {type: 'group'},
+        {type: 'event'},
+        {type: 'material'},
+        {type: 'timeline'}
+      ]
+    }
+  ]
 }
