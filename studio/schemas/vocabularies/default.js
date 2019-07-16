5f151dad6c0589be5e0e012a880ca3624cefff22
Tarje Lavik <tarje.lavik@gmail.com>
2019-07-17 00:49:40 +0200

    Fix error in rights url

--- studio/schemas/vocabularies/default.js
+++ studio/schemas/vocabularies/default.js
@@ -1,73 +1,73 @@
 export const supportedLanguages = [
-    { id: 'nor', title: 'Norwegian', isDefault: true },
-    { id: 'sme', title: 'Sami' },
-    { id: 'eng', title: 'English' },
-    { id: 'ger', title: 'German' },
+  {id: 'nor', title: 'Norwegian', isDefault: true},
+  {id: 'sme', title: 'Sami'},
+  {id: 'eng', title: 'English'},
+  {id: 'ger', title: 'German'}
 ]
 
 export const languages = [
-    { title: 'Norwegian', value: 'nor' },
-    { title: 'Sami', value: 'sme' },
-    { title: 'English', value: 'eng' },
-    { title: 'German', value: 'ger' },
+  {title: 'Norwegian', value: 'nor'},
+  {title: 'Sami', value: 'sme'},
+  {title: 'English', value: 'eng'},
+  {title: 'German', value: 'ger'}
 ]
 
 export const rights = [
-    { title: 'Public Domain Mark', value: 'https://creativecommons.org/publicdomain/mark/1.0/' },
-    { title: 'CC0 1.0 - Public Domain Dedication', value: 'https://creativecommons.org/publicdomain/zero/1.0/' },
-    { title: 'CC BY', value: 'https://creativecommons.org/licenses/by/4.0/"' },
-    { title: 'In copyright', value: 'http://rightsstatements.org/vocab/InC/1.0/' },
-    { title: 'In copyright - non-commercial use permitted', value: 'http://rightsstatements.org/vocab/InC-NC/1.0/' },
-    { title: 'Copyright not evaluated', value: 'http://rightsstatements.org/vocab/CNE/1.0/' },
-    { title: 'Copyright undetermined', value: 'http://rightsstatements.org/vocab/UND/1.0/' },
+  {title: 'Public Domain Mark', value: 'https://creativecommons.org/publicdomain/mark/1.0/'},
+  {title: 'CC0 1.0 - Public Domain Dedication', value: 'https://creativecommons.org/publicdomain/zero/1.0/'},
+  {title: 'CC BY', value: 'https://creativecommons.org/licenses/by/4.0/'},
+  {title: 'In copyright', value: 'http://rightsstatements.org/vocab/InC/1.0/'},
+  {title: 'In copyright - non-commercial use permitted', value: 'http://rightsstatements.org/vocab/InC-NC/1.0/'},
+  {title: 'Copyright not evaluated', value: 'http://rightsstatements.org/vocab/CNE/1.0/'},
+  {title: 'Copyright undetermined', value: 'http://rightsstatements.org/vocab/UND/1.0/'}
 ]
 
 export const reportTypes = [
-    { title: 'Condition assessment', value: 'conditionAssessment' },
-    { title: 'Book binding assessment', value: 'bookBindingAssessment' },
-    { title: 'Pest assessment', value: 'pestAssessment' },
-    { title: 'Conservation report', value: 'conservationReport' },
+  {title: 'Condition assessment', value: 'conditionAssessment'},
+  {title: 'Book binding assessment', value: 'bookBindingAssessment'},
+  {title: 'Pest assessment', value: 'pestAssessment'},
+  {title: 'Conservation report', value: 'conservationReport'}
 ]
 
 export const activityTypes = [
-    { title: 'Add', value: 'add' },
-    { title: 'Remove', value: 'remove' },
-    { title: 'Correspondance', value: 'correspondance' },
-    { title: 'Use', value: 'use' }
+  {title: 'Add', value: 'add'},
+  {title: 'Remove', value: 'remove'},
+  {title: 'Correspondance', value: 'correspondance'},
+  {title: 'Use', value: 'use'}
 ]
 
 export const units = [
-    { title: 'Centimeters', value: 'cm' },
-    { title: 'Meters', value: 'm' },
-    { title: 'Kilos', value: 'k' },
-    { title: 'Grams', value: 'gr' }
+  {title: 'Centimeters', value: 'cm'},
+  {title: 'Meters', value: 'm'},
+  {title: 'Kilos', value: 'k'},
+  {title: 'Grams', value: 'gr'}
 ]
 
 export const appelationTypes = [
-    { title: 'Preferred title', value: 'preferredTitle' },
-    { title: 'Translated title', value: 'translatedTitle' },
-    { title: 'Preferred name', value: 'preferredName' },
-    { title: 'Given name', value: 'givenName' },
-    { title: 'Stage name', value: 'stageName' },
-    { title: 'Pseudonym', value: 'pseudonym' },
-    { title: 'Alternativ name', value: 'alternativeName' },
-    { title: 'Secular name', value: 'secularName' },
-    { title: 'Religious name', value: 'religiousName' },
-    { title: 'Nickname', value: 'nickname' }
+  {title: 'Preferred title', value: 'preferredTitle'},
+  {title: 'Translated title', value: 'translatedTitle'},
+  {title: 'Preferred name', value: 'preferredName'},
+  {title: 'Given name', value: 'givenName'},
+  {title: 'Stage name', value: 'stageName'},
+  {title: 'Pseudonym', value: 'pseudonym'},
+  {title: 'Alternativ name', value: 'alternativeName'},
+  {title: 'Secular name', value: 'secularName'},
+  {title: 'Religious name', value: 'religiousName'},
+  {title: 'Nickname', value: 'nickname'}
 ]
 
 export const textTypes = [
-    { title: 'Description', value: 'description' },
-    { title: 'Brief text', value: 'briefText' },
+  {title: 'Description', value: 'description'},
+  {title: 'Brief text', value: 'briefText'}
 ]
 
-export default { 
-    supportedLanguages,
-    languages,
-    reportTypes,
-    activityTypes,
-    units,
-    appelationTypes,
-    textTypes,
-    rights
-};
+export default {
+  supportedLanguages,
+  languages,
+  reportTypes,
+  activityTypes,
+  units,
+  appelationTypes,
+  textTypes,
+  rights
+}
