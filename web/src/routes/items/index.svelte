02eb79278e0c878a667d4e3e7d671b761f659af1
Tarje Lavik <tarje.lavik@gmail.com>
2019-07-22 11:18:34 +0200

    Better items grid

--- web/src/routes/items/index.svelte
+++ web/src/routes/items/index.svelte
@@ -30,40 +30,41 @@
 <style>
 	main {
 		position: relative;
-		max-width: 56em;
-		background-color: white;
+		background-color: #222;
 		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
 
+	main > h1 {
+		color: white;
+	}
+
 	section {
 		display: grid;
-		grid-template-columns: repeat(auto-fill, minmax(350px, 1fr));
+		grid-template-columns: repeat(1, minmax(250px, 1fr));
+		gap: 1em;
 	}
 
 	article {
 		display: flex;
-		flex-direction: row;
+		flex-direction: column;
 		justify-content: flex-start;
 		align-items: flex-start;
     box-sizing: border-box;
-		background-color: rgb(65, 63, 63);
-		color: white;
-    margin: 0 1em 2em 1em;
-		padding: 1em;
-		box-shadow: 0px 5px 10px rgba(0,0,0,0.15);
+		background-color: #333;
+		color: #ccc;
+		box-shadow: 0px 5px 10px rgba(0,0,0,0.95);
 	}
 	.content h1 {
-		font-size: 1.4rem;
+		font-size: 1rem;
 	}
 	img {
 		width: 100%;
-		box-shadow: 0px 5px 10px rgba(0,0,0,0.75);
 	}
 
 	.image {
-		width: 50%;
+		width: 100%;
 	}
 
 	.content {
@@ -71,17 +72,21 @@
 		width: 50%;
 	}
 
-	/* @media screen and (min-width: 40em) {
-    article {
-       max-width: 100%;
-    }
+	@media screen and (min-width: 40em) {
+		section {
+			display: grid;
+			grid-template-columns: repeat(2, minmax(250px, 1fr));
+			gap: 1em;
+		}
 	}
 	
 	@media screen and (min-width: 60em) {
-		article {
-			max-width: calc(50% - 2em);
+		section {
+			display: grid;
+			grid-template-columns: repeat(3, minmax(250px, 1fr));
+			gap: 1em;
 		}
-	} */
+	}
 </style>
 
 <svelte:head>
@@ -98,7 +103,7 @@
 				waiting for the 'click' event -->
 		<article>
 			<a class="image" rel='prefetch' href='id/{item._id}'>
-				<img src={urlFor(item.mainRepresentation).height(200).url()} />
+				<img src={urlFor(item.mainRepresentation).height(350).url()} />
 			</a>
 			<div class="content">
 				<h1><a rel='prefetch' href='id/{item._id}'>{item.label}</a></h1>
