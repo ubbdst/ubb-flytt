4df6637699bdc49ce370d46f256cf6ed438f4fd8
Tarje Lavik <tarje.lavik@gmail.com>
2019-08-13 22:41:06 +0200

    Somewhat nices footer

--- web/src/components/Footer.svelte
+++ web/src/components/Footer.svelte
@@ -1,6 +1,6 @@
 <style>
   footer {
-    background-color: black;
+    background-color: #222;
     color: #666;
     border-top: 1px solid #333;
     padding: 2em;
@@ -8,5 +8,5 @@
 </style>
 
 <footer>
- Hei hei :-)
+ <p>Laget av <a href="id/21d46f66-0b8f-4e5c-9532-c2c7fee22fb9">Tarje Sælen Lavik</a></p>
 </footer>
