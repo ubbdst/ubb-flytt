b086c0f3e65330c52644744b42c1be899378ac89
Tarje Lavik <tarje.lavik@gmail.com>
2019-11-17 15:33:24 +0100

    Fix indentation

--- studio/schemas/documents/work.js
+++ studio/schemas/documents/work.js
@@ -1,21 +1,21 @@
 export default {
-    title: 'Work',
-    name: 'work',
-    type: 'document',
-    fields: [
-        {
-            title: 'Title',
-            name: 'title',
-            type: 'string',
-        },
-        {
-            title: 'Activity stream',
-description: 'Events and activities connected to this object',
-            name: 'events',
-            type: 'array',
-            of: [
-                { type: 'creation' }
-            ]
-        },
-    ]
+  title: 'Work',
+  name: 'work',
+  type: 'document',
+  fields: [
+    {
+      title: 'Title',
+      name: 'title',
+      type: 'string'
+    },
+    {
+      title: 'Activity stream',
+      description: 'Events and activities connected to this object',
+      name: 'events',
+      type: 'array',
+      of: [
+        {type: 'creation'}
+      ]
+    }
+  ]
 }
