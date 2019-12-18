fca6388a3b44705cd9d27932ce0e7bc8d74576d2
Tarje Lavik <tarje.lavik@gmail.com>
2019-12-18 11:14:54 +0100

    Update sanity and functions experimentz

--- studio/functions/addTitle.js
+++ studio/functions/addTitle.js
@@ -7,20 +7,24 @@ const client = sanityClient({
   // Remember to add a token with write access to your Netlify app’s environment variables.
   // Call it SANITY_TOKEN
   // https://docs.netlify.com/configure-builds/environment-variables/#declare-variables
-  token: process.env.SANITY_TOKEN,
+  token: process.env.SANITY_FUNCTION_TOKEN,
   useCdn: false
 })
+
+const query = `*[_type == "move" ] { carriedOutBy[]->, "movedCount": count(moved), movedFrom->, movedTo->, timespan }`
 
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 // $ sanity hook create add-missing-value production https://yourdomain.netlify.com/.netlify/functions/update-sanity-content
 // Webhooks can also be added in the API settings on https://manage.sanity.io
 exports.handler = async (event, context) => {
   const {body} = event
-  const {created} = body.ids // get the ids of new documents
+  const bodyParsed = JSON.parse(body)
+  const {created} = bodyParsed.ids // get the ids of new documents
+  console.log(created)
 
   try {
-    const res = await created.reduce((trans, _id) => trans.patch(_id).setIfMissing({
-      title: 'Missing TITLE!!!'
+    const res = await created.map((trans, _id) => trans.patch(_id).setIfMissing({
+      label: 'Missing TITLE!!!'
     }), client.transaction()).commit().catch(console.error)
     console.log(`Updated ${res.length} documents.`)
     return {
